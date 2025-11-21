de13add89de976fd3229f0a3dd25e3eb4bc95062
アリメクタ アレネクト <alimektor@gmail.com>
2025-11-21 06:23:21 +0300

    chore: Format code for run.py

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ import toml
 from datetime import date
 import argparse
 
+
 def main(debug):
     today = date.today()
     markdown_template = open("config/TEMPLATE.md").read()
@@ -21,24 +22,42 @@ def main(debug):
     with open("README.md", "w", encoding="utf-8") as readme_file:
         print(readme_content, file=readme_file)
 
+
 def get_github_projects():
-    gh_command = "gh repo list --json owner,name,description,url,isArchived,isPrivate,isFork"
+    gh_command = (
+        "gh repo list --json owner,name,description,url,isArchived,isPrivate,isFork"
+    )
     result = subprocess.run(gh_command.split(" "), capture_output=True, text=True)
     projects = json.loads(result.stdout)
     return projects
 
+
 def get_public_github_projects():
     projects = get_github_projects()
-    return [project for project in projects if not project["isArchived"] and not project["isPrivate"] and not project["isFork"] and not project["name"] == "Alimektor" and not project["name"] == "alimektor.github.io"]
+    return [
+        project
+        for project in projects
+        if not project["isArchived"]
+        and not project["isPrivate"]
+        and not project["isFork"]
+        and not project["name"] == "Alimektor"
+        and not project["name"] == "alimektor.github.io"
+    ]
+
 
 def get_archive_github_projects():
     projects = get_github_projects()
-    return [project for project in projects if project["isArchived"] and not project["isPrivate"] and not project["isFork"]]
+    return [
+        project
+        for project in projects
+        if project["isArchived"] and not project["isPrivate"] and not project["isFork"]
+    ]
+
 
 if __name__ == "__main__":
     try:
         parser = argparse.ArgumentParser("run.py")
-        parser.add_argument("--debug", "-d", action='store_true')
+        parser.add_argument("--debug", "-d", action="store_true")
         args = parser.parse_args()
         main(args.debug)
     except json.decoder.JSONDecodeError:
